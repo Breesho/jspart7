6d3c207ae0461dc4d19ca581ead9151bc036101a
Breesho <felix.noel3@orange.fr>
2020-05-04 17:02:30 +0200

    puhs

--- Exercice-1/assets/js/app.js
+++
@@ -1,9 +0,0 @@
-let nbr1 = document.getElementById('firstNumber');
-let nbr2 = document.getElementById('secondNumber');
-let result = document.getElementById('answer');
-let button = document.getElementById('myButton');
-function multiplication() {
-    result.value =(nbr1.value * nbr2.value).toString();
-}
-
-button.addEventListener ('click', multiplication);
